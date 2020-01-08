afa33867a5ae7db3f6afdff42e3a113b17a4705b
Meet mehta <shubhamqwerty1490@gmail.com>
2020-01-09 03:49:45 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 01:10:19 2020
+
+@author: Meet Mehta
+"""
+#Our first program in python is hello world. 
+#To print a value we use print() command
+print("Hello World")
