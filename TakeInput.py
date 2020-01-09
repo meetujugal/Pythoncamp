8074648a6f3b7f54d5aa3c34d4b105b2cd8dfae9
Meet mehta <shubhamqwerty1490@gmail.com>
2020-01-10 00:20:58 +0530

    Add files via upload

---
+++ TakeInput.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 00:12:12 2020
+
+@author: MEET MEHTA
+"""
+# In this program we will take value from user and print the value.
+# we will use input() command to take values.
+
+x=input("Enter your Name:")
+print(x)
+y=input("Enter your Age:")
+print(y)
+
+# Output
+
+#   Enter your Name:Meet Mehta
+#   Meet Mehta
+
+#   Enter your Age:21
+#   21
