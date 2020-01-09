2f66f49d1d2e33d782b65e2c0d5cc070f13e69be
Meet mehta <shubhamqwerty1490@gmail.com>
2020-01-10 00:11:34 +0530

    Add files via upload

---
+++ DMAS.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 00:08:16 2020
+
+@author: MEET MEHTA
+"""
+#perform basic DMAS operation on 2 numbers.
+x=4
+y=2
+print(x+y)
+print(x*y)
+print(x-y)
+print(x/y)
+print(x**y)
