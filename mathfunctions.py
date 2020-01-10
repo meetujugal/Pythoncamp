ff1fcf74e8352cd3b6b7bccfa8e63b101f26a4bd
Meet mehta <shubhamqwerty1490@gmail.com>
2020-01-11 00:18:35 +0530

    Add files via upload

---
+++ mathfunctions.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 23:21:51 2020
+
+@author: MEET MEHTA
+"""
+# In this program we will see some of the mathematical functions associated.
+# math library contains different functions.
+
+import math
+a=2.2
+b=-2.7
+c=6
+d=3
+print("\n")# to print a new line
+print("The ceiling value for a = ",math.ceil(a))# ceiling function to print the upper value of the number
+print("\n")
+print("The floor value for a = ",math.floor(a))# floor function to print the lower value of the number
+print("\n")
+print("The absolute value of b = ",math.fabs(b))# absolute value of a number
+print("\n")
+print("The Factorial of c = ",math.factorial(c))# factorial value of a number
+print("\n")
+print("The Copysigned value of -5 and 6 =",math.copysign(5,-6))# copies sign of b to a
+print("\n")
+print("The gcd of c and d =",math.gcd(c,d))
+print("\n")
+print("The exponential value of e^c=",math.exp(c))# natural e**c
+print("\n")
+print("The Logarithm value of log 2 to the base 3 =",math.log(2,3))# log value
+print("\n")
+print("The cosine value of a is",math.cos(a))#cosine value of a 
+print("\n")
+print("The sine value of c =", math.sin(c))#sine value of c
+print("\n")
+print("The tan value of c =",math.tan(c))
+print("\n")
+
+
+#Output
+
+#The ceiling value for a =  3
+
+#The floor value for a =  2
+
+#The absolute value of b =  2.7
+
+#The Factorial of c =  720
+
+#The Copysigned value of -5 and 6 = -5.0
+
+#The gcd of c and d = 3
+
+#The exponential value of e^c= 403.4287934927351
+
+#The Logarithm value of log 2 to the base 3 = 0.6309297535714574
+
+#The cosine value of a is -0.5885011172553458
+
+#The sine value of c = -0.27941549819892586
+
+#The tan value of c = -0.29100619138474915
