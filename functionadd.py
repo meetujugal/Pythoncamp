5f61100a24230002aba66b29341ddb4128c05bd4
Meet mehta <shubhamqwerty1490@gmail.com>
2020-01-19 03:28:13 +0530

    added a simple description of function

---
+++ functionadd.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 03:21:03 2020
+
+@author: MEET MEHTA
+
+"""
+#In this simple program we have demonstrated how to define a function in python using def
+# def(parameter): is used to define a function
+#to call a function we simply use the function name with the parameter we want to pass as argument
+# the advantage of function is we can call a function anytime
+
+import numpy as np
+def add(a,b):
+    answer=a+b
+    print(answer)
+    
+add(5,6)
+    
+#output 
+#11
